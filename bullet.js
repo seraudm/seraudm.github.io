1afb3b6b540ebd57536a914d694d0f36be73c384
seraudm <mael.seraud@gmail.com>
2024-11-27 16:12:22 +0100

    Création de la fonction draw tank

--- bullet.js
+++ bullet.js
@@ -3,10 +3,13 @@ let numberBullets = 0;
 const bulletDefaultSpeed = 1;
 
 class bullet {
-    constructor (X,Y, angle ){
-        this.angle = angle
+    constructor (X,Y, angle){
+        this.created = false;
+        this.angle = angle;
         this.position = {x: X, y:Y};
         this.nbBounces = 0;
         this.speed = bulletDefaultSpeed;
+        this.id = "bullet" + numberBullets;
+        numberBullets ++;
     }
 }
--- tank.js
+++ tank.js
@@ -1,3 +1,45 @@
 let listTanks = [];
 let numberTanks = 0;
 
+class tank{
+    constructor (X,Y, angle, color){
+        this.created = false;
+        this.angle = angle;
+        this.position = {x: X, y:Y};
+        this.color = color;
+        this.id = "tank" + numberTanks;
+        numberTanks ++;
+    }
+}
+
+function drawTank(){
+    for (tank of listTanks){
+        if (tank.created){
+            const tankHtml = document.createElement("div");
+            const turretHtml  = document.createElement("div");
+
+            turretHtml.className = "turret";
+            tankHtml.className = "tank";
+            tankHtml.id = tank.id;
+
+            tankHtml.style.left = `${tank.position.x}px`;
+            tankHtml.style.top = `${tank.position.y}px`;
+
+
+            tankHtml.style.transform = `translate(-50%, -50%) rotate(${tank.angle}deg)`;
+
+            game.appendChild(tankHtml);
+            tankHtml.appendChild(turretHtml);
+        } else {
+            const tankHtml = document.getElementById(tank.id);
+
+            tankHtml.style.left = `${tank.position.x}px`;
+            tankHtml.style.top = `${tank.position.y}px`;
+
+            tankHtml.style.transform = `translate(-50%, -50%) rotate(${tank.angle}deg)`;
+
+            
+        }
+
+    }
+}
