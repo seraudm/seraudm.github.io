dc47babf397764c71929cce1604f16028751e7ed
seraudm <mael.seraud@gmail.com>
2024-11-27 15:51:27 +0100

    Ajout de la classe bullet

--- bullet.js
+++ bullet.js
@@ -1,3 +1,12 @@
 let listBullets = [];
 let numberBullets = 0;
+const bulletDefaultSpeed = 1;
 
+class bullet {
+    constructor (X,Y, angle ){
+        this.angle = angle
+        this.position = {x: X, y:Y};
+        this.nbBounces = 0;
+        this.speed = bulletDefaultSpeed;
+    }
+}
