fe3af93435dbb02f0de96f0336d3e7cfc67878f1
seraudm <mael.seraud@gmail.com>
2024-12-19 16:47:30 +0100

    V2 Gestion des collisions entre les tanks et les murs

--- tank.js
+++ tank.js
@@ -6,7 +6,8 @@ class Tank{
     static DEFAULT_ANGLE_SPEED_DEG = 90; // Unit: deg/s
     static DEFAULT_ANGLE_SPEED = Tank.DEFAULT_ANGLE_SPEED_DEG*Math.PI/180; // Unit: rad/s
     static DEFAULT_SHOOTING_COOLDOWN = 0.75; // Unit: s
-    static DEFAULT_SIZE = 60; //Unit: GameUnit
+    static DEFAULT_SIZE = 60; //Unit: GameUnit CAREFULL THIS THE DIAMETER
+    static NUMBER_CHECKING_POINTS = 32;
     static numberTanks = 0;
 
     constructor (X,Y, angle, color, moveForward, moveBackward, turnLeft, turnRight, shoot, size=Tank.DEFAULT_SIZE){
@@ -40,7 +41,9 @@ class Tank{
     }
 
     updatePosition(dt){
+        // Calculate next position
         let nextPosition = {x: this.position.x, y:this.position.y};
+
         if (listKeysPressed.get(this.keybinds.moveForward)){
             nextPosition.x += Math.sin(this.angle) * this.speed *dt;
             nextPosition.y -= Math.cos(this.angle) * this.speed *dt;
@@ -49,14 +52,33 @@ class Tank{
             nextPosition.x -= Math.sin(this.angle) * this.speed *dt;
             nextPosition.y += Math.cos(this.angle) * this.speed *dt;
         }
+
+        // Verify the validity of the next postion, using NUMBER_CHECKING_POINT checking points around the tank and the center
+        let validity = isValid(nextPosition);
+        
+        for (let i=0; i<Tank.NUMBER_CHECKING_POINTS; i++){
+            let checkingPoint = {x: nextPosition.x, y: nextPosition.y}; // Copie de nextPosition pour ne pas modifier sa valeur
+            console.log(i,checkingPoint);
+            checkingPoint.x += Math.sin(this.angle + i * 2*Math.PI/Tank.NUMBER_CHECKING_POINTS) * this.size/2;
+            checkingPoint.y += Math.cos(this.angle + i * 2*Math.PI/Tank.NUMBER_CHECKING_POINTS) * this.size/2;
+            console.log(i,checkingPoint);
+            validity = validity && isValid(checkingPoint);
+        }
+
+        if (validity){
+            this.position = nextPosition;
+        }
+
+
+        this.updateAngle(dt);
+    }
+
+    updateAngle(dt){
         if (listKeysPressed.get(this.keybinds.turnLeft)){
             this.angle -= this.angleSpeed *dt;
         }
         if (listKeysPressed.get(this.keybinds.turnRight)){
             this.angle += this.angleSpeed *dt;
-        }
-        if (isValid(nextPosition)){
-            this.position=nextPosition;
         }
     }
 
