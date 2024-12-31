cd5644aaa6108d06b8638903030bacf808c447b3
seraudm <mael.seraud@gmail.com>
2024-12-31 17:32:31 +0100

    Correction musique 3

--- jeu_tank.js
+++ jeu_tank.js
@@ -5,68 +5,73 @@ const CELLS_COLOURS = [new Colour(255, 255, 255, 255), new Colour(0, 0, 0, 255),
 const GAME_MAP = GameMap.getMapByID("lake");
 
 const MUSIC = new Audio();
-MUSIC.loop = true;
-MUSIC.src = "audio/musique.mp3";
-MUSIC.autoplay = true;
-MUSIC.addEventListener("loadeddata", () => MUSIC.play());
-MUSIC.load();
-
-
 let listKeysPressed = new Map();
-listKeysPressed.set("z",false);
-listKeysPressed.set("q",false);
-listKeysPressed.set("s",false);
-listKeysPressed.set("d",false);
-listKeysPressed.set(" ",false);
-listKeysPressed.set("ArrowLeft",false);
-listKeysPressed.set("ArrowRight",false);
-listKeysPressed.set("ArrowDown",false);
-listKeysPressed.set("ArrowUp",false);
-listKeysPressed.set("0",false);
 
-let currentTime= Date.now();
+document.addEventListener('DOMContentLoaded', () => {
 
-function main(){
-    GAME_MAP.load();
-    Tank.addTank(new Tank(GAME_MAP.spawns[0], 0,"rgb(255,0,0)","z","s","q","d"," "));
-    Tank.addTank(new Tank(GAME_MAP.spawns[1],0,"rgb(255, 238, 0)","ArrowUp","ArrowDown","ArrowLeft","ArrowRight","0"));
-}
+    MUSIC.loop = true;
+    MUSIC.src = "audio/musique.mp3";
+    MUSIC.autoplay = true;
+    MUSIC.addEventListener("loadeddata", () => MUSIC.play());
+    MUSIC.load();
 
-main();
 
-function tick(){
-    let nextTime = Date.now();
-    let dt = (nextTime - currentTime)/1000; //Delta between two ticks in s
-    currentTime = nextTime;
+    listKeysPressed.set("z",false);
+    listKeysPressed.set("q",false);
+    listKeysPressed.set("s",false);
+    listKeysPressed.set("d",false);
+    listKeysPressed.set(" ",false);
+    listKeysPressed.set("ArrowLeft",false);
+    listKeysPressed.set("ArrowRight",false);
+    listKeysPressed.set("ArrowDown",false);
+    listKeysPressed.set("ArrowUp",false);
+    listKeysPressed.set("0",false);
     
-    Tank.updateTanksPosition(dt);
-    Bullet.updateBulletsPosition(dt);
-    Tank.shootTanks(dt);
-    collisionsBulletsTanks();
-    collisionsBulletsBullets();
-    Bullet.drawBullets();
-    Tank.drawTanks();
-}
+    let currentTime= Date.now();
 
-setInterval(tick, 10);
-
-
-// Mise a jour des touches pressées
-document.addEventListener("keydown", (e) => {
-    if (listKeysPressed.has(e.key)){
-        listKeysPressed.set(e.key, true);
+    function main(){
+        GAME_MAP.load();
+        Tank.addTank(new Tank(GAME_MAP.spawns[0], 0,"rgb(255,0,0)","z","s","q","d"," "));
+        Tank.addTank(new Tank(GAME_MAP.spawns[1],0,"rgb(255, 238, 0)","ArrowUp","ArrowDown","ArrowLeft","ArrowRight","0"));
     }
-});
 
-document.addEventListener("keyup", (e) => {
-    if (listKeysPressed.has(e.key)){
-        listKeysPressed.set(e.key, false);
+    main();
+
+    function tick(){
+        let nextTime = Date.now();
+        let dt = (nextTime - currentTime)/1000; //Delta between two ticks in s
+        currentTime = nextTime;
+        
+        Tank.updateTanksPosition(dt);
+        Bullet.updateBulletsPosition(dt);
+        Tank.shootTanks(dt);
+        collisionsBulletsTanks();
+        collisionsBulletsBullets();
+        Bullet.drawBullets();
+        Tank.drawTanks();
     }
+
+    setInterval(tick, 10);
+
+
+    // Mise a jour des touches pressées
+    document.addEventListener("keydown", (e) => {
+        if (listKeysPressed.has(e.key)){
+            listKeysPressed.set(e.key, true);
+        }
+    });
+
+    document.addEventListener("keyup", (e) => {
+        if (listKeysPressed.has(e.key)){
+            listKeysPressed.set(e.key, false);
+        }
+    });
+
+
+    function resizeMap(){
+        GAME_MAP.updateSize();
+    }
+
+    window.addEventListener("resize", resizeMap);
+
 });
-
-
-function resizeMap(){
-    GAME_MAP.updateSize();
-}
-
-window.addEventListener("resize", resizeMap);
